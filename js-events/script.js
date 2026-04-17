4a9a03cd9775e1c62c495b86b668acfe75962f48
José <rmzspencerjose@outlook.com>
2026-04-17 17:29:22 +0000

    class 19

---
+++ js-events/script.js
@@ -0,0 +1,9 @@
+//function greet(name) {
+    //console.log("Hello "+ name);
+//}
+//greet("Miku");
+//function addNumbers(num1, num2) {
+    //let sum = num1 + num2;
+    //alert("Sum is equal to " + sum);
+//}
+//addNumbers(6,7);
