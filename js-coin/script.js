5af189284e1bd5e5693ba4c008aec54afe724db7
José <rmzspencerjose@outlook.com>
2026-04-28 16:40:39 +0000

    class 25

--- js-coin/script.js
+++ js-coin/script.js
@@ -1,3 +1,15 @@
 function random() {
     let number = Math.random();
+    console.log(number);
+    if (number >= 0.5) {
+        let head = document.querySelector("#span");
+        span.innerHTML = "Heads";
+        let image = document.querySelector("#headtail");
+        image.style.backgroundImage = "url('https://static.wikia.nocookie.net/the-fireball760/images/f/fb/Normal.png/revision/latest?cb=20250301063441')";
+    } else if (number < 0.5) {
+        let tail = document.querySelector("#span");
+        span.innerHTML = "Tails";
+        let image = document.querySelector("#headtail");
+        image.style.backgroundImage = "url('https://static.wikia.nocookie.net/the-fireball760/images/7/7a/Easy.png/revision/latest/thumbnail?cb=20250301063416')";
+    }
 }
