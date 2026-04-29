763296ea5091bb6d6a96c8c571893fd1dac9d5f0
José <rmzspencerjose@outlook.com>
2026-04-29 20:20:48 +0000

    class 26

--- js-coin/script.js
+++ js-coin/script.js
@@ -5,11 +5,11 @@ function random() {
         let head = document.querySelector("#span");
         span.innerHTML = "Heads";
         let image = document.querySelector("#headtail");
-        image.style.backgroundImage = "url('https://static.wikia.nocookie.net/the-fireball760/images/f/fb/Normal.png/revision/latest?cb=20250301063441')";
+        image.style.backgroundImage = "url('https://assets1.ignimgs.com/2018/06/21/hollowknight-1280-1529623462572_160w.jpg?width=1280&format=jpg&auto=webp&quality=80')";
     } else if (number < 0.5) {
         let tail = document.querySelector("#span");
         span.innerHTML = "Tails";
         let image = document.querySelector("#headtail");
-        image.style.backgroundImage = "url('https://static.wikia.nocookie.net/the-fireball760/images/7/7a/Easy.png/revision/latest/thumbnail?cb=20250301063416')";
+        image.style.backgroundImage = "url('https://media.vandal.net/i/1200x1200/8-2025/26/202582613201212_1.jpg')";
     }
 }
