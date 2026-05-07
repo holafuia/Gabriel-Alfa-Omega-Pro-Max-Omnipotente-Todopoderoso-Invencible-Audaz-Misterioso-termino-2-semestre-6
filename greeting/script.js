92f22a3cf17ae821a2f900f27472e2c8ba863be3
José <rmzspencerjose@outlook.com>
2026-05-07 17:30:06 +0000

    class 29

--- greeting/script.js
+++ greeting/script.js
@@ -5,8 +5,20 @@
 // }
 document.addEventListener("DOMContentLoaded", function(){
     document.querySelector("form").onsubmit = function() {
-        let name = document.querySelector("#name).value;
-            console.log("Hi " + name);
-            return false;
+        let name = document.querySelector("#name").value;
+        document.querySelector("h1").innerHTML = "Hi " + name;
+        let time = new Date();
+        let hour = time.getHours();
+        if (hour < 12) {
+            document.querySelector("#h2").innerHTML = "Good morning";
+        } else if (hour < 18) {
+            document.querySelector("#h2").innerHTML = "Good afternoon";
+        } else {
+            document.querySelector("#h2").innerHTML = "Good evening";
+        }
+        let day = time.getDay();
+        console.log(day);
+        // array
+        return false;
     }
 })
