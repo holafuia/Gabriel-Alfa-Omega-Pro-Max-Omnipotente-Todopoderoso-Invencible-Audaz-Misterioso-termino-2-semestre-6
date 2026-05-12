267d1f92ccc658c3749d203dca1e1b8f119232cf
José <rmzspencerjose@outlook.com>
2026-05-12 16:39:00 +0000

    class 32

--- intervals/script.js
+++ intervals/script.js
@@ -1,23 +1,39 @@
+let counter;
 document.addEventListener("DOMContentLoaded", function() {
-    document.querySelector("#1").onclick = function(){
-        let counter = 60;
+    document.querySelector("#button1").onclick = function(){
+        counter = 60;
     }
-    document.querySelector("#5").onclick = function(){
-        let counter = 300;
+    document.querySelector("#button2").onclick = function(){
+        counter = 300;
     }
-    document.querySelector("#10").onclick = function (){
-        let counter = 600;
+    document.querySelector("#button3").onclick = function (){
+        counter = 600;
     }
-})
 
-function count() {
-    counter--;
-    document.querySelector('h1').innerHTML = counter;
-}
+    let count = function() {
+        counter--;
+        document.querySelector("h1").innerHTML = counter;
+    }
 
-document.addEventListener('DOMContentLoaded', function() {
     document.querySelector("#start").onclick = function(){
     setInterval(count, 1000);
     document.querySelector("#start").disabled = true;
-    };
-});
+    }
+
+    const alarm = new Audio 
+    if (counter == 0) {
+
+    }
+})
+
+// function count() {
+//     counter++;
+//     document.querySelector('h1').innerHTML = counter;
+// }
+
+// document.addEventListener('DOMContentLoaded', function() {
+//     document.querySelector("#start").onclick = function(){
+//     setInterval(count, 1000);
+//     document.querySelector("#start").disabled = true;
+//     };
+// });
